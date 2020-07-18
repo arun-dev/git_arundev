f291659c42321a963a66a6f3b83b7b9310aa00e2
Arundev <arundev143@gmail.com>
2020-07-18 01:11:26 +0100

    cahnges for list iteration

---
+++ test/src/main/java/com/interview/collection/hash/NthMostRareElementList.java
@@ -0,0 +1,34 @@
+package com.interview.collection.hash;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NthMostRareElementList {
+    public static void main(String[] args) {
+        int[] array = {2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 16};
+        //IntStream.range(0, 8).forEach(i -> System.out.println(findRarest(array, i)));
+        IntStream.range(0, 1).forEach(i -> System.out.println(rare(array, i)));
+    }
+
+    public static long findRarest(int[] array, int nth) {
+        Map<Integer, Long> map = Arrays.stream(array).boxed().collect(
+                Collectors.groupingBy(arrayElement -> arrayElement, Collectors.counting()));
+        Comparator<Map.Entry<Integer, Long>> comp = Comparator.comparing(entry -> entry.getValue());
+        List<Map.Entry<Integer, Long>> list = new ArrayList<>(map.entrySet());
+        list.sort(comp);
+        if (nth > list.size() - 1) throw new IllegalArgumentException("no such element");
+        return list.get(nth).getKey();
+    }
+
+    public static long rare(int array[], int nth) {
+        Map<Integer, Long> map = Arrays.stream(array).boxed().collect(
+                Collectors.groupingBy(element ->
+                 {
+                    System.out.println(element);
+                    return element;
+                }, Collectors.counting()));
+        return 0l;
+    }
+
+}
